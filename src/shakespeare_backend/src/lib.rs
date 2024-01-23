bc5d3e24a3b2f9d7687f18667a3af2efefafb2b0
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2024-01-23 11:24:00 +0000

    init

--- src/shakespeare_backend/src/lib.rs
+++ src/shakespeare_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use ic_cdk::api::time;
+
+const COMPLETE_WORKS: &str = include_str!("../../../shakespeare.txt");
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn line() -> String {
+    let start_index = time() as usize % COMPLETE_WORKS.len();
+    let truncated = &COMPLETE_WORKS[start_index..];
+    let mut lines = truncated.split('\n');
+    // First chunk is an incomplete line.
+    lines.next();
+    let mut result = String::new();
+    for _ in 0..5 {
+        result.push_str(lines.next().unwrap());
+    }
+    result
 }
